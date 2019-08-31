678083d69523e328e8b57f8832bc3d782437d186
nm-t <nathaliamtan@gmail.com>
2019-08-31 12:58:46 +1000

    [FEAT] Added main.js with basic rounding function

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * roundNumber(number)
+ * @param {*} number Number to be rounded
+ * Returns input rounded up (if >= 0.5), or down
+ */
+function roundNumber(number) {
+    let remainder = number % 1;
+
+    if (remainder >= 0.5) {
+        number = Math.ceil(number);
+    }
+    else {
+        number = Math.floor(number);
+    }
+
+    return number;
+}
